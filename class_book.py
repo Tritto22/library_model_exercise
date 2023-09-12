8dcde66ebd999fdfe47160ec6f3998b53820957f
Simone <tritto.simone@gmail.com>
2023-09-12 20:06:00 +0200

    create_classes

---
+++ class_book.py
@@ -0,0 +1,6 @@
+class Book:
+    def __init__(self, title, author, genre):
+        self.title=title
+        self.title=title
+        self.title=title
+        
---
+++ class_library.py
@@ -0,0 +1,6 @@
+import class_book
+
+class Library:
+    def __init__(self):
+        self.catalogue=[]
+        self.genres=dict()
